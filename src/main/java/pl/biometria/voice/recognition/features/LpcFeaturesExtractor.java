50af608f82b3a7abcce296f9e6fadc57e0231a85
Matt <grafiszti@gmail.com>
2015-12-15 20:12:45 +0100

    refactoring

--- src/main/java/pl/biometria/voice/recognition/features/LpcFeaturesExtractor.java
+++ src/main/java/pl/biometria/voice/recognition/features/LpcFeaturesExtractor.java
@@ -1,51 +1,50 @@
 package pl.biometria.voice.recognition.features;
 
 
-import pl.biometria.voice.algorithms.LinearPredictiveCoding;
-import pl.biometria.voice.algorithms.windowing.HammingWindowFunction;
-import pl.biometria.voice.algorithms.windowing.WindowFunction;
+import pl.biometria.voice.recognition.algorithms.LinearPredictiveCoding;
+import pl.biometria.voice.recognition.algorithms.windowing.HammingWindowFunction;
+import pl.biometria.voice.recognition.algorithms.windowing.WindowFunction;
 
-public class LpcFeaturesExtractor
-        extends WindowedFeaturesExtractor<double[]> {
+public class LpcFeaturesExtractor extends WindowedFeaturesExtractor<double[]> {
 
-    private final int poles;
-    private final WindowFunction windowFunction;
-    private final LinearPredictiveCoding lpc;
+  private final int poles;
+  private final WindowFunction windowFunction;
+  private final LinearPredictiveCoding lpc;
 
-    public LpcFeaturesExtractor(float sampleRate, int poles) {
-        super(sampleRate);
-        this.poles = poles;
-        this.windowFunction = new HammingWindowFunction(windowSize);
-        this.lpc = new LinearPredictiveCoding(windowSize, poles);
+  public LpcFeaturesExtractor(float sampleRate, int poles) {
+    super(sampleRate);
+    this.poles = poles;
+    this.windowFunction = new HammingWindowFunction(windowSize);
+    this.lpc = new LinearPredictiveCoding(windowSize, poles);
+  }
+
+  @Override
+  public double[] extractFeatures(double[] voiceSample) {
+
+    double[] voiceFeatures = new double[poles];
+    double[] audioWindow = new double[windowSize];
+
+    int counter = 0;
+    int halfWindowLength = windowSize / 2;
+
+    for (int i = 0; (i + windowSize) <= voiceSample.length; i += halfWindowLength) {
+
+      System.arraycopy(voiceSample, i, audioWindow, 0, windowSize);
+
+      windowFunction.applyFunction(audioWindow);
+      double[] lpcCoeffs = lpc.applyLinearPredictiveCoding(audioWindow)[0];
+
+      for (int j = 0; j < poles; j++) {
+        voiceFeatures[j] += lpcCoeffs[j];
+      }
+      counter++;
     }
 
-    @Override
-    public double[] extractFeatures(double[] voiceSample) {
-
-        double[] voiceFeatures = new double[poles];
-        double[] audioWindow = new double[windowSize];
-
-        int counter = 0;
-        int halfWindowLength = windowSize / 2;
-
-        for (int i = 0; (i + windowSize) <= voiceSample.length; i += halfWindowLength) {
-
-            System.arraycopy(voiceSample, i, audioWindow, 0, windowSize);
-
-            windowFunction.applyFunction(audioWindow);
-            double[] lpcCoeffs = lpc.applyLinearPredictiveCoding(audioWindow)[0];
-
-            for (int j = 0; j < poles; j++) {
-                voiceFeatures[j] += lpcCoeffs[j];
-            }
-            counter++;
-        }
-
-        if (counter > 1) {
-            for (int i = 0; i < poles; i++) {
-                voiceFeatures[i] /= counter;
-            }
-        }
-        return voiceFeatures;
+    if (counter > 1) {
+      for (int i = 0; i < poles; i++) {
+        voiceFeatures[i] /= counter;
+      }
     }
+    return voiceFeatures;
+  }
 }
